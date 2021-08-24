872a84de9fddc52892668193b467a6740f932359
andrewkopalchik <73274955+andrewkopalchik@users.noreply.github.com>
2021-08-24 19:35:08 +0300

    Update h.mm.ss.cpp

--- h.mm.ss.cpp
+++ h.mm.ss.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	while (true)
+	{
+		cin >> n;
+		cout << n / 3600 % 24 << ':' << n / 60 % 60 / 10 << n / 60 % 10 << ":" << n % 60 / 10 << n % 60 % 10;
+	}
+	return 0;
+}
 
